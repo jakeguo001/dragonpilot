9468fed58f3b051311bcc5e88b26e4a5b5e9ce3f
jakeguo001 <70319379+jakeguo001@users.noreply.github.com>
2020-12-03 23:33:23 +0800

    Update main.c

--- panda/board/main.c
+++ panda/board/main.c
@@ -718,9 +718,14 @@ void TIM1_BRK_TIM9_IRQ_Handler(void) {
       puts("EON hasn't sent a heartbeat for 0x");
       puth(heartbeat_counter);
       puts(" seconds. Safety is set to SILENT mode.\n");
-      if (current_safety_mode != SAFETY_SILENT) {
-        set_safety_mode(SAFETY_SILENT, 0U);
+      if (current_safety_mode != SAFETY_NOOUTPUT) {
+        set_safety_mode(SAFETY_NOOUTPUT, 0U);
       }
+      // Volvo cuts power to panda. Need CAN alive 
+      // to switch without relay.
+      //if (power_save_status != POWER_SAVE_STATUS_ENABLED) {
+      //  set_power_save_state(POWER_SAVE_STATUS_ENABLED);
+      //}
       if (power_save_status != POWER_SAVE_STATUS_ENABLED) {
         set_power_save_state(POWER_SAVE_STATUS_ENABLED);
       }
@@ -823,7 +828,8 @@ int main(void) {
   // use TIM2->CNT to read
 
   // init to SILENT and can silent
-  set_safety_mode(SAFETY_SILENT, 0);
+  //set_safety_mode(SAFETY_SILENT, 0);
+  set_safety_mode(SAFETY_NOOUTPUT, 0U);
 
   // enable CAN TXs
   current_board->enable_can_transcievers(true);
